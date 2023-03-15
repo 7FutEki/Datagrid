4a32e9afa7e7bfe9734020158d2523b2c93d8234
7FutEki <105148135+7FutEki@users.noreply.github.com>
2023-03-15 12:34:42 +0400

    Add files via upload

---
+++ DataGrid-master/DataGrid/Window1.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataGrid
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+
+        public Teacher Teacher { get; set; }
+      
+        public Window1()
+        {
+            InitializeComponent();
+           
+
+        }
+        public Teacher GetNewObject()
+        {
+            return new Teacher() { Name = "Новый предмет", ContactNumber = "Новый номер" };
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            Teacher teacher = new Teacher()
+            {
+                Name = tb_fio.Text,
+                ContactNumber = tb_number.Text
+            };
+
+            Teacher = teacher; 
+            Close();
+
+        }
+      
+    }
+
+}
